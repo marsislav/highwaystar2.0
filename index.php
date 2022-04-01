6017d3fe325d69112150163f23c0d6414188900d
miroslav kostadinov (marsislav) <mkostadinov87@gmail.com>
2022-04-01 20:36:06 +0300

    Version 0.02 - file organization

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
                 <h2>
                     <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
                 </h2>
-                <div>Posted on: <a href="<?php echo get_permalink();?>"> <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date();?> </time> </a>
-            By: <a href="<?php echo get_author_posts_url(get_the_author_meta('ID'))?>"> <?php echo get_the_author();?></a>
-            </div>
+                <div>
+                    <?php hs_post_meta();?>
+                </div>
             <div>
             <?php the_excerpt();?>
             </div>
-            <a href="<?php echo get_the_permalink();?>" title="<?php the_title_attribute(); ?>">Read more... <span class="u-screen-reder-text">About <?php the_title();?></span></a>
+            <?php hs_readmore_link();?>
             <?php }?>
             <?php the_posts_pagination(); ?>
             
