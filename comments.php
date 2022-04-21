dbf79bd29ad8535cba87bedb244d11b1dca668f2
miroslav kostadinov (marsislav) <mkostadinov87@gmail.com>
2022-04-21 15:37:34 +0300

    Comments fallback

--- comments.php
+++ comments.php
@@ -1,34 +1,35 @@
 <?php
 if (post_password_required()) {
-     return;
+    return;
 
-}?>
+} ?>
 <div id="comments" class="comment-form">
-<?php
-if (have_comments()) {?>
-<h2 class="comment-title">
-    <?php 
-    printf(
-    esc_html(_n('%1$s Reply to "%1$s"',
-    '%1$s Replies to "%1$s"', get_comments_number(), '_themename'
-    )),
-    number_format_i18n(get_comments_number(), get_the_title())
-    )?>
-</h2>
-<ul class="comment-list">
-<?php wp_list_comments(array(
-'avatar_size'=>200,
-'reply_text'=>'Reply',
-'callback'=>'_themename_comment_callback'
-));?>
-</ul>
-<?php the_comments_pagination ();?>
-<?php } ?>
-<?php 
-if (!comments_open() && get_comments_nuber() ) {?>
-<p class="comments-closed">
-    <?php esc_html_e('Comments are closed for this post', '_themename'); ?>
-</p>
-<?php }?>
-<?php comment_form ();?>
+    <?php
+    if (have_comments()) { ?>
+        <h2 class="comment-title">
+            <?php
+            printf(
+                esc_html(_n('%1$s Reply to "%1$s"',
+                    '%1$s Replies to "%1$s"', get_comments_number(), '_themename'
+                )),
+                number_format_i18n(get_comments_number(), get_the_title())
+            ) ?>
+        </h2>
+        <ul class="comment-list">
+            <?php wp_list_comments(array(
+                'short_ping'=>false,
+                'avatar_size' => 200,
+                'reply_text' => 'Reply',
+                'callback' => '_themename_comment_callback'
+            )); ?>
+        </ul>
+        <?php the_comments_pagination(); ?>
+    <?php } ?>
+    <?php
+    if (!comments_open() && get_comments_nuber()) { ?>
+        <p class="comments-closed">
+            <?php esc_html_e('Comments are closed for this post', '_themename'); ?>
+        </p>
+    <?php } ?>
+    <?php comment_form(); ?>
 </div>
--- lib/comment-callback.php
+++ lib/comment-callback.php
@@ -1,8 +1,9 @@
 <?php
 function _themename_comment_callback($comment, $args, $depth)
 {
+    $tag=($args['style']==='div') ? 'div' : 'li';
     ?>
-<li id="<?php comment_ID(); ?>" <?php comment_class(['comment', $comment->comment_parent ? 'comment-child' : '']) ?>>
+<<?php echo $tag; ?> id="<?php comment_ID(); ?>" <?php comment_class(['comment', $comment->comment_parent ? 'comment-child' : '']) ?>>
     <article class="comment-body" id="div-comment-<?php comment_ID(); ?>">
         <?php if($args['avatar_size']!=0) echo get_avatar($comment, $args['avatar_size'], false, false, array('class' => 'comment-avatar')) ?>
         <?php edit_comment_link(esc_html__('Edit Comment', '_themename'), '<span class="comment-edit-link">', '</span>'); ?>
@@ -18,15 +19,19 @@ function _themename_comment_callback($comment, $args, $depth)
             <?php if ($comment->comment_approved=='0') {?>
     <p class="comment-awaiting"><?php esc_html_e('Your comment is awaiting moderation', '_themename') ;?></p>
     <?php } ?>
-        <?php comment_text();?>
+        <?php
+        if ($comment->comment_type=='' || (( $comment->comment_type=='pingback' || $comment->comment_type== 'trackback')
+            && !$args['short_ping'])) {
+            comment_text();
+            }
+        ?>
             <?php
-                comment_reply_link(array (
+                comment_reply_link(array_merge($args, array(
                       'depth'=>$depth,
-                      'max_depth'=>$args['max_depth'],
                       'add_bellow'=>'div-comment',
                       'before'=>'<div class="comment-reply-link">',
                       'after'=>'</div>'
-                ))
+                )));
             ?>
         </div>
     </article>
